7b288aeb3d0e9947100a21eb58c1b91da1064848
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-08-01 18:36:51 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ let expdate = document.getElementById('expdate')
 let monthyear = document.getElementById('monthyear')
 let cvc = document.getElementById('cvc')
 let confirmButton = document.getElementById('confirmButton')
+let cardmonth = document.getElementById('cardmonth')
+let cardyear = document.getElementById('cardyear')
+// let cardnumber = document.getElementById('cardnumber');
+// let numbers = document.getElementById('numbers');
 
+
+cardname.addEventListener('input', function() {
+    name.textContent = cardname.value;
+});
+
+cardnumber.addEventListener('input', function() {
+    numbers.textContent = cardnumber.value;
+});
+cvc.addEventListener('input', ()=>{
+    gray.textContent = cvc.value
+   
+})
+expdate.addEventListener('input', ()=>{
+    cardmonth.textContent = expdate.value
+})
+monthyear.addEventListener('input', ()=>{
+    cardyear.textContent = monthyear.value
+})
+
+confirmButton.addEventListener('click', function() {
+    nameDiv.textContent = cardname.value;
+    numbers.textContent = cardnumber.value;
+});
